354510f9514a2c885c263ae7161014186b142a44
TkhiienLok <tkhiien@gmail.com>
2019-06-15 18:58:26 +0300

    added looking up all posts of a specific user

--- js/getInfo.js
+++ js/getInfo.js
@@ -32,7 +32,47 @@ httpGet("https://jsonplaceholder.typicode.com/users")
 	error => alert(`Rejected: ${error}`)
   );
   
+var usersAmount;
+/* getting JSON of all users  */
 function getUsers(res){
-    const users = res;
-    alert(users);
+    const users = JSON.parse(res);
+    alert(users.length);
+	usersAmount = users.length;
 }
+
+/* getting title and txt of a specific user, id is taken from text input */
+window.onload = function(){
+    document.getElementById("open").onclick = function fun(){
+	    var x = document.forms["myForm"]["user_id"].value;
+		console.log(x,typeof(x),Number(x) > 0 && Number(x) <= usersAmount);
+		
+		var theurl2 = "https://jsonplaceholder.typicode.com/posts?userId=" + x;
+		
+		httpGet(theurl2)
+		.then(
+			function(response){			
+				var response1 = JSON.parse(response);
+				for(var item in response1) {
+					
+					var div = document.createElement('div');
+					div.className = "title";
+					div.innerHTML = response1[item].title;
+					posts.appendChild(div);
+					
+					var div = document.createElement('div');
+					div.className = "body";
+					div.innerHTML = response1[item].body;
+					posts.appendChild(div);
+								
+					
+				}
+						
+			},
+			error => alert(`Rejected: ${error}`)
+		)
+		
+		
+	    
+	}
+}
+
